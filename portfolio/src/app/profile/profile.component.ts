ca5b43978e211eee35d51261aa1f2d7ef8690bcb
ravirawat0 <ravirawat0@outlook.com>
2024-06-05 14:59:05 +0530

    f: Completed the Profile card

--- portfolio/src/app/profile/profile.component.ts
+++ portfolio/src/app/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
