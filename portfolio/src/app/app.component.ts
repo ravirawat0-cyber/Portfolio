5820c612699152c2bd7f5ed3e70cf080804c1453
ravirawat0 <ravirawat0@outlook.com>
2024-06-07 00:44:55 +0530

    f: implemented navebar feature, and added project component.

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {ProfileComponent} from "./profile/profile.component";
 import {AboutComponent} from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileComponent, AboutComponent],
+  imports: [RouterOutlet, ProfileComponent, AboutComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
