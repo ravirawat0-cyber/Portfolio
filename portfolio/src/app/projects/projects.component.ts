d7d1bda4930bb3bc83cd3d72c2e187299364797f
ravirawat0 <ravirawat0@outlook.com>
2024-06-09 19:00:03 +0530

    f:completed project page.

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatTooltip
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
